a8c30038c1da9a703e8f8d043b323710db0aeff5
Samuel <samuelsantanadasilva8@gmail.com>
2023-09-11 12:43:26 -0300

    Atualizando

--- src/section/SobreMim/SobreMim.jsx
+++ src/section/SobreMim/SobreMim.jsx
@@ -5,14 +5,9 @@ export function SobreMim(){
         <>
         <div id="SobreMim"className={Style.div}>
             <h1>Sobre Mim</h1>
-            <p>
-                 Olá, meu nome é Samuel Santana, tenho 19 anos 👦 e sou um desenvolvedor web 💻 e de Chatbots 🤖 altamente motivado, sempre em busca de aprimoramento contínuo . Estou investindo no aprendizado do inglês  e no aprimoramento das minhas habilidades em aplicativos mobile 📱. Na minha área de atuação, utilizo a linguagem de programação JavaScript, TypeScript e o framework React para criar sistemas web inovadores 💡. Minha experiência inclui projetos voluntários envolvendo Chatbots e desenvolvimento de sites, o que me proporcionou um valioso conhecimento prático 🧠. Estou constantemente buscando expandir meu networking profissional. Atualmente, sou estagiário no banco BRB, onde, embora minhas atividades não estejam diretamente relacionadas à minha área de atuação, trabalho diariamente com documentos 📄 e planilhas , o que contribui para o desenvolvimento de um conjunto de habilidades abrangente 💪.
-            </p>
 
             <p>
-                Estou disponível e pronto para abraçar novas oportunidades! Sou um desenvolvedor web front-end junior apaixonado por criar experiências incríveis na web. Minhas habilidades incluem o desenvolvimento de planilhas dinâmicas e interativas, bem como a criação de chatbots inteligentes que melhoram a comunicação e a experiência do usuário. Com minha dedicação, curiosidade e vontade de aprender, estou pronto para contribuir em projetos desafiadores que impulsionam a inovação tecnológica. 💪
-
-                Recrutador, permita-me demonstrar meu potencial e paixão pelo desenvolvimento web e juntos construiremos algo incrível! 😉 Espero que isso capte a atenção dos recrutadores e mostre sua motivação e habilidades como desenvolvedor web front-end junior. Se você precisar de mais ajuda, é só me avisar! 🙌
+              Olá! Sou Samuel Santana, um desenvolvedor web e de Chatbots de 19 anos, dedicado ao aprimoramento constante. Estou investindo no inglês e em habilidades de aplicativos móveis, utilizando JavaScript, TypeScript e React para criar sistemas web inovadores. Minha experiência inclui projetos voluntários com Chatbots e desenvolvimento de sites, proporcionando um conhecimento prático valioso. Atualmente, sou estagiário no BRB, onde aprimoro minhas habilidades em documentos e planilhas. Estou ansioso por novas oportunidades para criar experiências incríveis na web e contribuir para a inovação tecnológica. Recrutadores, estou pronto para mostrar meu potencial e paixão pelo desenvolvimento web. 😉
             </p>
         </div>
         </>
