b3423323164b6d3da68353087a5876c68ed97959
Samuel <samuelsantanadasilva8@gmail.com>
2023-11-28 12:20:44 -0300

    Atualizando o sass

--- src/section/Experiencia/Experiencia.jsx
+++ src/section/Experiencia/Experiencia.jsx
@@ -1,20 +1,24 @@
-import  './Experiencia.scss'
+import  './Experiencia.scss';
 import { FormaçaoEexperiencia } from './formaçaoEexperiencia/formaçaoEexperiencia'
 
 export function Experiencia(){
     return(
         <section id="Oportunidades" className="sectionExperiencia">
             <div>
-                <h3 className='text-center'>Experiencia</h3>
-                <p className='text-center'>
-                Minha trajetória profissional inclui uma experiência como estagiário na DNA System, atuando como desenvolvedor, além de outro estágio no Banco BRB. Também dediquei parte do meu tempo como desenvolvedor voluntário, contribuindo para a área de chatbots e sites. Atualmente, estou focado exclusivamente em meu trabalho como desenvolvedor freelancer, iniciando meu próprio negócio na página  
-                    <a 
-                    href="https://www.instagram.com/techworld" 
-                    className='text-decoration-none fw-bold text-primary'>
-                    Tech World
-                </a>
-                , cujo link pode ser encontrado logo abaixo ou na palavra tech world.
-                </p>
+                <div>
+                    <h3 className='text-center'>Experiencia</h3>
+                    <p className='text-center'>
+                    Minha trajetória profissional inclui uma experiência como estagiário na DNA System, atuando como desenvolvedor, além de outro estágio no Banco BRB. Também dediquei parte do meu tempo como desenvolvedor voluntário, contribuindo para a área de chatbots e sites. Atualmente, estou focado exclusivamente em meu trabalho como desenvolvedor freelancer, iniciando meu próprio negócio na página
+                        <a
+                        href="https://www.instagram.com/techworld"
+                        className='text-decoration-none fw-bold text-primary'>
+                        Tech World
+                    </a>
+                    , cujo link pode ser encontrado logo abaixo ou na palavra tech world.
+                    </p>
+                </div>
+
+                <hr />
 
                 <div>
                        <FormaçaoEexperiencia />
--- src/section/SobreMim/SobreMim.jsx
+++ src/section/SobreMim/SobreMim.jsx
@@ -3,35 +3,36 @@ import  './SobreMim.scss'
 export function SobreMim(){
     return(
         <section id='SobreMim' className="sectionSobreMim">
-            <div  className='sobreMimdivPai'>
-            <h1 className='text-center'>Sobre Mim</h1>
 
-                <div className='divTextos d-flex '>
-                   
-                    <div className='col-6'>
-                        <h3>Frontend Developer</h3>
-                        <p>
-                        Olá! Sou Samuel Santana, um desenvolvedor web e de Chatbots, dedicado ao aprimoramento constante. Estou investindo no inglês e em habilidades de aplicativos móveis e chatboots utilizando <span className='negrito'>JavaScript</span>, <span className='negrito'>TypeScript</span> e <span className='negrito'>React</span> para criar sistemas web inovadores. Estou ansioso por novas oportunidades para criar experiências incríveis na web e contribuir para a inovação tecnológica.
-                        </p>
-                    </div>
-                    
-                    <div className='col-6'>
-                        <ul className='SobreMimexperienciaLista'>
-                            <li>
-                                <strong>Formado:</strong> 2021 - 2023
-                            </li>
-                            <li>
-                                <strong>Analise e Desenvolvimento de Sistemas</strong> 
-                            </li>
-                         </ul>
-                    </div>
-   
+            <h1 className='text-center textoh1'>Sobre Mim</h1>
+
+            <div className='divTexto d-flex'>
+
+                <div className='divTxt'>
+                    <h3 className='text-center'>Frontend Developer</h3>
+                    <p className='text-center'>
+                    Olá! Sou Samuel Santana, um desenvolvedor web e de Chatbots, dedicado ao aprimoramento constante. Estou investindo no inglês e em habilidades de aplicativos móveis e chatboots utilizando <span className='negrito'>JavaScript</span>, <span className='negrito'>TypeScript</span> e <span className='negrito'>React</span> para criar sistemas web inovadores. Estou ansioso por novas oportunidades para criar experiências incríveis na web e contribuir para a inovação tecnológica.
+                    </p>
                 </div>
 
-                <div>
-                    <img src="https://eopablo.github.io/pablocavalcante/images/codando.svg" alt="imagem" className='imagemSobreMim'/>
+                <span className='borda'></span>
+
+                <div className='divTxt'>
+                    <h3 className='text-center'>Educação</h3>
+                    <ul className='SobreMimexperienciaLista'>
+                        <li>
+                            <strong>Formado:</strong> 2021 - 2023
+                        </li>
+                        <li>
+                            <strong>Analise e Desenvolvimento de Sistemas</strong> 
+                        </li>
+                        <li>Sou formado na faculdade anhanguera onde aprendi conceitos de analise e desenvolvimentos de sistemas.</li>
+                    </ul>
                 </div>
-                
+            </div>
+
+            <div>
+                <img src="https://eopablo.github.io/pablocavalcante/images/codando.svg" alt="imagem" className='imagemSobreMim'/>
             </div>
         </section>
         
