5f633ce48c2b60a49bebd1246528220fab357b03
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-01-23 10:18:45 -0300

    Alterado os links de projetos

--- src/section/SectionProjetos/Javascript/Javascript.jsx
+++ src/section/SectionProjetos/Javascript/Javascript.jsx
@@ -10,37 +10,37 @@ export function CarrosselJavascript(){
 
                 titulo1="Pagina Pessoal Demonstrativa"
                 explicativo1="Pessoal Demonstrativo"
-                href1="https://samuel-santana109.github.io/PaginaPessoalCaptura/"
+                href1="https://samuelsantana-dev.github.io/PaginaPessoalCaptura/"
                 src1="https://images.pexels.com/photos/5926378/pexels-photo-5926378.jpeg?auto=compress&cs=tinysrgb&w=400"
 
                 titulo2="checkout_design"
                 explicativo2="Design de checkout em bootstrap"
-                href2="https://samuel-santana109.github.io/checkout_design/"
+                href2="https://samuelsantana-dev.github.io/checkout_design/"
                 src2="https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=600"
 
                 titulo3="Album_design"
                 explicativo3="Design de Albúm em bootstrap"
-                href3=" https://samuel-santana109.github.io/album_design/"
+                href3=" https://samuelsantana-dev.github.io/album_design/"
                 src3="https://images.pexels.com/photos/895228/pexels-photo-895228.jpeg?auto=compress&cs=tinysrgb&w=800"
 
                 titulo4="Projeto Cordel"
                 explicativo4="Site com efeito parallax."
-                href4="https://github.com/Samuel-Santana109/projeto-cordell"
+                href4="https://github.com/samuelsantana-dev/projeto-cordell"
                 src4={img4}
 
                 titulo5="Pagina de Captura"
                 explicativo5="Site das minhas redes sociais"
-                href5="https://samuel-santana109.github.io/paginaDeCaptura/"
+                href5="https://samuelsantana-dev.github.io/paginaDeCaptura/"
                 src5="https://images.pexels.com/photos/50987/money-card-business-credit-card-50987.jpeg?auto=compress&cs=tinysrgb&w=400"
 
                 titulo6="Rede Social"
                 explicativo6="Site das minhas redes sociais"
-                href6="https://samuel-santana109.github.io/Rede-Social/"
+                href6="https://samuelsantana-dev.github.io/Rede-Social/"
                 src6="https://images.pexels.com/photos/607812/pexels-photo-607812.jpeg?auto=compress&cs=tinysrgb&w=600"
 
                 titulo7="Blog_design"
                 explicativo7="Design de blog em bootstrap"
-                href7="https://samuel-santana109.github.io/Blog_design/"
+                href7="https://samuelsantana-dev.github.io/Blog_design/"
                 src7="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=600"
             />
         </>
