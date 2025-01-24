84884682ce8cd85e265c5511531b04d6df30f1e8
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-01-24 16:38:40 -0300

    Novos textos e removido comentarios

--- src/componentes/Header/textoAutomatico.jsx
+++ src/componentes/Header/textoAutomatico.jsx
@@ -1,38 +1,22 @@
 import  { useEffect, useRef } from 'react';
-//Essencialmente, useRef é como uma “caixa” que pode conter um valor mutável em sua propriedade . current . Você pode estar familiarizado com os refs principalmente como uma forma de acessar o DOM. Se você passar um objeto ref para React com <div ref = {myRef} /> , React definirá sua propriedade .
 import Typed from 'typed.js';
 
 export function TextAutomatico() {
-    // Cria uma referência para o elemento HTML onde o texto será digitado
     let el = useRef(null);
 
     useEffect(() => {
-        // Cria uma nova instância da biblioteca Typed.js no elemento referenciado por 'el'
         const typed = new Typed(el.current, {
-            // Define um array de strings que serão digitadas no elemento
-            strings: ['Front-end', 'Web Development', 'ChatBots'],
-
-            // Define a velocidade de digitação em milissegundos por caractere
+            strings: ['Front-end', 'React.js', 'Vue.js', 'Node.js', 'Full Stack'],
             typeSpeed: 100,
-            
-            // Define a velocidade de retrocesso (apagar) em milissegundos por caractere
             backSpeed: 50,
-            
-            // Define o atraso após completar a digitação antes de iniciar o retrocesso
             backDelay: 50,
-            
-            // Define se o processo de digitação e retrocesso deve ser repetido em um loop infinito
             loop: true
         });
 
-        // Função de retorno executada quando o componente é desmontado
-        // Garante que a instância Typed.js seja destruída para evitar vazamentos de memória
         return () => {
-            typed.destroy(); // Destroi a instância Typed.js
+            typed.destroy();
         };
-    }, []); // O array vazio indica que este efeito deve ser executado apenas uma vez (ao montar o componente)
-
-    // Renderiza um elemento <h3> com a classe 'text-js' e utiliza a referência 'el' para associá-lo ao Typed.js
+    }, []); 
     return <span className="text-js" ref={el}></span>
 }
 
