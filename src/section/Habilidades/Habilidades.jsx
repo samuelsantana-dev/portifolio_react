b3d46eaab5a33c31bbf86faebfc7269c0c5e2a6b
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-01-24 17:35:21 -0300

    Atualizada novas habilidades

--- src/section/Habilidades/Habilidades.jsx
+++ src/section/Habilidades/Habilidades.jsx
@@ -1,33 +1,28 @@
 import './Habilidades.scss'
 import {
     DiHtml5,
-    DiCss3,
     DiJsBadge,
     DiReact,
     DiBootstrap,
     DiTypo3,
-    DiSass,
     DiGit,
     DiNpm,
     DiCelluloid,
-    DiCss3Full,
     DiMysql
   } from "react-icons/di";
   
   const technologies = [
-    { id: "html", name: "HTML5", icon: <DiHtml5 />, text: "Linguagem de Marcação para Estruturação de Conteúdo na Web" },
-    { id: "css", name: "CSS3", icon: <DiCss3 />, text: "Linguagem de Estilização para Design de Páginas na Web" },
+    { id: "html", name: "HTML5 e CSS3", icon: <DiHtml5 />, text: "Linguagem de Marcação e Estilização para Desenvolvimento de Páginas Web" },
     { id: "js", name: "JavaScript", icon: <DiJsBadge />, text: "Linguagem de Programação para Desenvolvimento Web Interativo" },
-    { id: "react", name: "React", icon: <DiReact />, text: "Biblioteca JavaScript para Construção de Interfaces de Usuário" },
-    { id: "bootstrap", name: "Bootstrap 5", icon: <DiBootstrap />, text: "Framework CSS para Desenvolvimento Web Responsivo" },
+    { id: "react", name: "React.js", icon: <DiReact />, text: "Biblioteca JavaScript para Construção de Interfaces de Usuário" },
+    { id: "bootstrap", name: "Bibliotecas CSS", icon: <DiBootstrap />, text: "Sass e Less, Tailwind, Bootstrap" },
     { id: "typescript", name: "TypeScript", icon: <DiTypo3 />, text: "Superset do JavaScript com Tipagem Estática" },
-    { id: "sassless", name: "Sass e Less", icon: <DiSass />, text: "Pré-processadores CSS para Estilização Avançada" },
+    { id: "Vue.js", name: "Vue.js", icon: <DiCelluloid />, text: "Framework JavaScript para Desenvolvimento de Interfaces de Usuário" },
     { id: "git", name: "Git e GitHub",icon: <DiGit />, text: "Sistema de Controle de Versão e Plataforma de Colaboração para Código" },
     { id: "npxnpm", name: "Npx e Npm", icon: <DiNpm />, text: "Gerenciadores de Pacotes para Projetos JavaScript" },
-    { id: "dialogflow", name: "Dialogflow", icon: <DiCelluloid />, text: "Plataforma de Desenvolvimento de Chatbots e Assistentes Virtuais" },
-    { id: "tailwindui", name: "Tailwind CSS", icon: <DiCss3Full />, text: "Framework CSS Utilizado no Desenvolvimento Web" },
     { id: "reactNative", name: "React Native", icon: <DiReact />, text: "Framework para Desenvolvimento de Aplicativos Móveis Multiplataforma" },
-    { id: "wordpress", name: "WordPress", icon: <DiCelluloid />, text: "Plataforma de Gerenciamento de Conteúdo (CMS) para Sites e Blogs" }, { id: "banco de dados My Sql", name: "My Sql", icon: <DiMysql />, text: "Plataforma de Gerenciamento Banco de Dados" }
+     { id: "banco de dados", name: "Banco de dados", icon: <DiMysql />, text: "Bancos relacionais (MySQL, PostgreSQL) e não relacionais (MongoDB)."},
+     { id: "nodejs", name: "Node.js", icon: <DiCelluloid />,   text: "Framework JavaScript assíncrona e orientada a eventos"      },
 ];
 
   
